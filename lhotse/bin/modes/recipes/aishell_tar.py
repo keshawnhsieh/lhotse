4a979c8b05c07bcb1eae5ea72325040d8749c8b3
Keshawn Hsieh <keshawnhsieh@126.com>
2023-08-28 14:51:53 +0800

    add aishell tar recipe

--- lhotse/bin/modes/recipes/aishell_tar.py
+++ lhotse/bin/modes/recipes/aishell_tar.py
@@ -2,6 +2,7 @@ import click
 
 from lhotse.bin.modes import download, prepare
 from lhotse.recipes.aishell_tar import prepare_aishell_tar
+from lhotse.recipes.aishell import download_aishell
 from lhotse.utils import Pathlike
 
 __all__ = ["aishell_tar"]
@@ -14,3 +15,8 @@ def aishell_tar(corpus_dir: Pathlike, output_dir: Pathlike):
     """Aishell ASR data preparation."""
     prepare_aishell_tar(corpus_dir, output_dir=output_dir)
 
+@download.command(context_settings=dict(show_default=True))
+@click.argument("target_dir", type=click.Path())
+def aishell(target_dir: Pathlike):
+    """Aishell download."""
+    download_aishell(target_dir)
