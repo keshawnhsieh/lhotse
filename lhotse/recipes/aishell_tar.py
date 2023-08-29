badf26302bd08d0b7ece4b12d5ad31f1ec9b3fbd
Keshawn Hsieh <keshawnhsieh@126.com>
2023-08-29 14:04:08 +0800

    add aishell tar recipe

--- lhotse/recipes/aishell_tar.py
+++ lhotse/recipes/aishell_tar.py
@@ -268,8 +268,11 @@ def prepare_aishell_tar_mp(
 
         tar_path = corpus_dir / f"{part}"
         all_tars = sorted(tar_path.rglob("*.tar"))[:100]
-        with Pool() as pool:
-            result = list(tqdm(pool.imap(helper, all_tars), total=len(all_tars)))
+        # with Pool() as pool:
+        #     result = list(tqdm(pool.imap(helper, all_tars), total=len(all_tars)))
+        result = []
+        for tar in all_tars:
+            result.append(helper(tar))
         recordings = [r[0] for r in result]
         supervisions = [r[1] for r in result]
 
