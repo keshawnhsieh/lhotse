f8752d9af64107803a25772deb0eb42fcb108c92
Keshawn Hsieh <keshawnhsieh@126.com>
2023-08-29 13:51:09 +0800

    add aishell tar recipe

--- lhotse/recipes/aishell_tar.py
+++ lhotse/recipes/aishell_tar.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from typing import Dict, Optional, Union
 
 from tqdm.auto import tqdm
+from multiprocessing import Pool
 
 from lhotse import validate_recordings_and_supervisions
 from lhotse.audio import Recording, RecordingSet
@@ -164,6 +165,113 @@ def prepare_aishell_tar(
                 supervisions.append(segment)
             stream.close()
             ftar.close()
+
+        recording_set = RecordingSet.from_recordings(recordings)
+        supervision_set = SupervisionSet.from_segments(supervisions)
+        validate_recordings_and_supervisions(recording_set, supervision_set)
+
+        if output_dir is not None:
+            supervision_set.to_file(
+                output_dir / f"aishell_supervisions_{part}.jsonl.gz"
+            )
+            recording_set.to_file(output_dir / f"aishell_recordings_{part}.jsonl.gz")
+
+        manifests[part] = {"recordings": recording_set, "supervisions": supervision_set}
+
+    return manifests
+
+def helper(tar):
+    recordings = []
+    supervisions = []
+    ftar = open(tar, 'rb')
+    stream = tarfile.open(fileobj=ftar, mode="r|*")
+    prev_prefix = None
+    example = {}
+    valid = True
+    for i, tarinfo in enumerate(stream):
+        name = tarinfo.name
+        pos = name.rfind('.')
+        assert pos > 0
+        prefix, postfix = name[:pos], name[pos + 1:]
+        if prev_prefix is not None and prefix != prev_prefix:
+            example['key'] = prev_prefix
+            if valid:
+                recording = example["recording"]
+                segment = example["segment"]
+                recordings.append(recording)
+                supervisions.append(segment)
+            example = {}
+            valid = True
+
+        with stream.extractfile(tarinfo) as file_obj:
+
+            if postfix == 'txt':
+                text = file_obj.read().decode('utf8').strip()
+                recording = example["recording"]
+                example["segment"] = SupervisionSegment(
+                    id=prefix,
+                    recording_id=prefix,
+                    start=0.0,
+                    duration=recording.duration,
+                    channel=0,
+                    language="Chinese",
+                    speaker="_".join(prefix.split("_")[:-1]),
+                    text=text.strip(),
+                )
+            elif postfix in AUDIO_FORMAT_SETS:
+                example["recording"] = Recording.from_tar(file_obj.read(), recording_id=prefix, tar_path=str(tar),
+                                                          tar_idx=i)
+            else:
+                example[postfix] = file_obj.read()
+
+        prev_prefix = prefix
+    if prev_prefix is not None:
+        recording = example["recording"]
+        segment = example["segment"]
+        recordings.append(recording)
+        supervisions.append(segment)
+    stream.close()
+    ftar.close()
+    return recordings, supervisions
+
+def prepare_aishell_tar_mp(
+    corpus_dir: Pathlike, output_dir: Optional[Pathlike] = None
+) -> Dict[str, Dict[str, Union[RecordingSet, SupervisionSet]]]:
+    """
+    Returns the manifests which consist of the Recordings and Supervisions
+    :param corpus_dir: Pathlike, the path of the data dir.
+    :param output_dir: Pathlike, the path where to write the manifests.
+    :return: a Dict whose key is the dataset part, and the value is Dicts with the keys 'recordings' and 'supervisions'.
+    """
+    corpus_dir = Path(corpus_dir)
+    assert corpus_dir.is_dir(), f"No such directory: {corpus_dir}"
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+    # transcript_path = corpus_dir / "data_aishell/transcript/aishell_transcript_v0.8.txt"
+    # transcript_dict = {}
+    # with open(transcript_path, "r", encoding="utf-8") as f:
+    #     for line in f.readlines():
+    #         idx_transcript = line.split()
+    #         content = " ".join(idx_transcript[1:])
+    #         content = text_normalize(content)
+    #         transcript_dict[idx_transcript[0]] = content
+    manifests = defaultdict(dict)
+    dataset_parts = ["train_l"]#, "dev", "test"]
+    for part in tqdm(
+        dataset_parts,
+        desc="Process aishell audio, it takes about 102 seconds.",
+    ):
+        logging.info(f"Processing aishell subset: {part}")
+        # Generate a mapping: utt_id -> (audio_path, audio_info, speaker, text)
+
+
+        tar_path = corpus_dir / f"{part}"
+        all_tars = sorted(tar_path.rglob("*.tar"))
+        with Pool() as pool:
+            result = list(tqdm(pool.imap(helper, all_tars), total=len(all_tars)))
+        recordings = [r[0] for r in result]
+        supervisions = [r[1] for r in result]
 
         recording_set = RecordingSet.from_recordings(recordings)
         supervision_set = SupervisionSet.from_segments(supervisions)
